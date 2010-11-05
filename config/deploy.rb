810ec9dd95982b85697662da54ce7e759611688a
Pete <peterlaus81@hotmail.com>
2010-11-05 18:10:34 +0100

    prod DB

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,27 +6,27 @@ set :use_sudo, false
 
 default_run_options[:pty] = true
 set :scm, :git
-#set :git_enable_submodules, 1
+set :git_enable_submodules, 1
 
 set :scm_passphrase, "M!pete0324"
 set :user, "PClaus"
 
 # use local copy
-# use local copy
-set :deploy_via, :copy                
+# set :deploy_via, :copy       
+#set :deploy_via, :remote_cache         
 set :copy_strategy, :export
 set :copy_compression, :zip
-#set :copy_cache, true
+set :copy_cache, true
 set :copy_exclude, ['.git']
 set :repository, "git@github.com:PClaus/#{application}.git"
 
 role :app, 'active-dating.eu'
 role :web, 'active-dating.eu'
-#role :db,  'active-dating.eu', :primary => true
+role :db,  'active-dating.eu', :primary => true
 
 set :user, 'ssh-300699-pclaus'
 ssh_options[:port] = '22'
-set :ssh_options, { :forward_agent => true }
+#set :ssh_options, { :forward_agent => true }
 
 # Restart mod_rails
 #namespace :deploy do
